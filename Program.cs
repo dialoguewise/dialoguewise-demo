73bd40387cfa341c34605debbb6ec83fb3e7c7e8
Ken Thomas <ken.tms@gmail.com>
2018-09-29 16:38:36 +1000

    Created sample project that integrates Dialogue Wise.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using DialogueWise.Client;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace DialogueWiseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of wheels");
+            var input = Console.ReadLine();
+
+            int.TryParse(input, out var wheels);
+
+            var request = new DialogueWiseRequest()
+            {
+                ApiKey = "b1266377591c4f2a9494c3abdd2cac5381D6Z825D26CEBAE8B6rn",
+                DialogueName = "demo-dialogue",
+                EmailHash = "/kgmM46s1xC56BOFWRZp4j+0bdU19URpXdNT9liAX50=",
+                VariableList = new Dictionary<string, object>()
+                {
+                    {"@wheel",wheels}
+                }
+            };
+
+            var dialogueWise = new DialogueWiseClient();
+            var response = dialogueWise.GetDialogue(request);
+            var jObject = JObject.Parse(response.Result);
+            var content = jObject["dialogue"];
+
+            Console.WriteLine(content);
+            Console.ReadLine();
+        }
+    }
+}
